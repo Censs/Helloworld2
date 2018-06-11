1df9a5e7970389802482f803b71a74b590ca1ee3
eclipse_cens <eclipse_email>
2018-06-11 09:45:52 +0800

    test conflict 111111111

--- src/main/java/com/cens/git/Test.java
+++ src/main/java/com/cens/git/Test.java
@@ -2,6 +2,6 @@ package com.cens.git;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("test???.....sdsd");
+		System.out.println("test???.....111111111");
 	}
 }
